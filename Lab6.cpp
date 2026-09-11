cc6bd9f01e0068fc4f21ce30c29f335223612d84
Mystery525 <avilaed525@gmail.com>
2026-09-11 16:31:34 -0700

    Initial Commit

---
+++ Lab6.cpp
@@ -0,0 +1,16 @@
+/*
+Write a C++ function enterArrayData() that receives a dynamic double array and populates it with values that the user enters via the console.
+
+Write a function outputArrayData() that receives a dynamic double array and outputs its contents on one line.
+
+Write a function sumArray() that receives a dynamic double array and both calculates and returns its sum. There is no output in this function.
+
+In main(), using dynamic memory allocation, create an array of doubles of size 5. Use a constant for this integer literal. Pass the array to the three functions to populate, output, and sum the array.
+
+Use pointer notation over array notation for this assignment.
+*/
+
+//Eduardo Avila
+//COMSC - 210 - 5293
+//Lab 6 - Dynamic Arrays and Functions 1
+
