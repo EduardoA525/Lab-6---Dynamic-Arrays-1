752ceecb22defc1c9a6ddb07e6abe74ddb27e0b8
Mystery525 <avilaed525@gmail.com>
2026-09-11 16:41:51 -0700

    Set up for functions

--- Lab6.cpp
+++ Lab6.cpp
@@ -1,11 +1,16 @@
 /*
-Write a C++ function enterArrayData() that receives a dynamic double array and populates it with values that the user enters via the console.
+Write a C++ function enterArrayData() that receives a dynamic double array and 
+populates it with values that the user enters via the console.
 
-Write a function outputArrayData() that receives a dynamic double array and outputs its contents on one line.
+Write a function outputArrayData() that receives a dynamic double array and 
+outputs its contents on one line.
 
-Write a function sumArray() that receives a dynamic double array and both calculates and returns its sum. There is no output in this function.
+Write a function sumArray() that receives a dynamic double array and both calculates and returns its sum.
+There is no output in this function.
 
-In main(), using dynamic memory allocation, create an array of doubles of size 5. Use a constant for this integer literal. Pass the array to the three functions to populate, output, and sum the array.
+In main(), using dynamic memory allocation, create an array of doubles of size 5. 
+Use a constant for this integer literal. 
+Pass the array to the three functions to populate, output, and sum the array.
 
 Use pointer notation over array notation for this assignment.
 */
@@ -14,3 +19,27 @@ Use pointer notation over array notation for this assignment.
 //COMSC - 210 - 5293
 //Lab 6 - Dynamic Arrays and Functions 1
 
+#include <iostream>
+
+using namespace std;
+
+//Size for array of doubles
+const int MAX_SIZE = 5;
+
+//Prototypes
+
+//enterArrayData()
+//outputArrayData()
+//sumArray()
+
+int main(){
+
+    //In main, create array of doubles of 5 using a const variable. This array to be used in functions
+
+
+
+    return 0;
+}
+
+
+//Definitions
