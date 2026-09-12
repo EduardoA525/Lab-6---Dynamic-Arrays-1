30027478508b90569a181715bbea378e05e10dcd
Mystery525 <avilaed525@gmail.com>
2026-09-11 17:21:12 -0700

    Fixed. Third function done. Needs output clean up

--- Lab6.cpp
+++ Lab6.cpp
@@ -1,12 +1,3 @@
-/*
-Write a function sumArray() that receives a dynamic double array and both calculates and returns its sum.
-There is no output in this function.
-
-Pass the array to the three functions to populate, output, and sum the array.
-
-Use pointer notation over array notation for this assignment.
-*/
-
 //Eduardo Avila
 //COMSC - 210 - 5293
 //Lab 6 - Dynamic Arrays and Functions 1
@@ -21,19 +12,19 @@ const int MAX_SIZE = 5;
 //Prototypes
 void enterArrayData(double *infoArray, int size);
 void outputArrayData(double *infoArray, int size);
-double sumArray(double *infoArray, int size)
+double sumArray(double *infoArray, int size);
 
 int main(){
 
-    //Creates array of doubles with size 5
+    //Creates super cool array of doubles with size 5
     double *infoArray = new double[MAX_SIZE];
 
-    //Calls functions
+    //Calls all the awesome functions
     enterArrayData(infoArray, MAX_SIZE);
     outputArrayData(infoArray, MAX_SIZE);
 
     double sum = sumArray(infoArray, MAX_SIZE);
-    //Output for the epic sum of the array
+    //Output the epic sum of the array
     cout << "Sum of all values: " << sum << endl;
 
     return 0;
@@ -44,7 +35,6 @@ int main(){
 void enterArrayData(double *infoArray, int size){
 
     cout << "Data entry for the array: " << endl;
-
     //Loop to input each item in the array
     for(int i = 0; i < size; i++){
         
@@ -60,7 +50,6 @@ void enterArrayData(double *infoArray, int size){
 void outputArrayData(double *infoArray, int size){
     
     cout << "Outputting array elements: ";
-
     //Loops to output each element
     for (int i = 0; i < size; i++){
 
@@ -73,7 +62,6 @@ void outputArrayData(double *infoArray, int size){
 double sumArray(double *infoArray, int size){
 
     double sum = 0;
-
     for (int i = 0; i < size; i++){
 
         //Same concept except it keeps adding up all elements
