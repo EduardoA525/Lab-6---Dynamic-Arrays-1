7f6089f12b173649be8dca60d9651737bb3935d3
Mystery525 <avilaed525@gmail.com>
2026-09-11 17:01:48 -0700

    First function done

--- Lab6.cpp
+++ Lab6.cpp
@@ -1,15 +1,10 @@
 /*
-Write a C++ function enterArrayData() that receives a dynamic double array and 
-populates it with values that the user enters via the console.
-
 Write a function outputArrayData() that receives a dynamic double array and 
 outputs its contents on one line.
 
 Write a function sumArray() that receives a dynamic double array and both calculates and returns its sum.
 There is no output in this function.
 
-In main(), using dynamic memory allocation, create an array of doubles of size 5. 
-Use a constant for this integer literal. 
 Pass the array to the three functions to populate, output, and sum the array.
 
 Use pointer notation over array notation for this assignment.
@@ -23,11 +18,10 @@ Use pointer notation over array notation for this assignment.
 
 using namespace std;
 
-//Size for array of doubles
+//Size for array
 const int MAX_SIZE = 5;
 
 //Prototypes
-
 void enterArrayData(double *infoArray, int size);
 //outputArrayData()
 //sumArray()
@@ -36,6 +30,9 @@ int main(){
 
     //Creates array of doubles with size 5
     double *infoArray = new double[MAX_SIZE];
+
+    //Calls functions
+    enterArrayData(infoArray, MAX_SIZE);
 
     return 0;
 }
@@ -48,7 +45,10 @@ void enterArrayData(double *infoArray, int size){
 
     //Loop to input each item in the array
     for(int i = 0; i < size; i++){
-
+        
+        cout << " > Element #" << i << ": ";
+        //As i goes up, so does the item slot
+        cin >> *(infoArray + i);
     }
 
     cout << "Data entry complete." << endl;
